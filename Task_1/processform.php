3d025cb37b4242e4010918b636241a6e14a543d1
Timothy Mayor <timothymayor24@gmail.com>
2020-03-26 08:05:21 +0100

    modified files

--- Task_1/processform.php
+++ Task_1/processform.php
@@ -1,5 +1,30 @@
 <?php 
 
-print_r($_POST)
+// print_r($_POST)
+
+if(isset($_POST['first_name'],  
+    ['last_name']  
+    ['email'] 
+    ['department'] 
+    ['gender'] 
+    ['message']
+)) { 
+    $dataOne = $_POST['first_name'];
+    $dataTwo = $_POST['last_name'];
+    $dataThree = $_POST['email'];
+    $dataFour = $_POST['department'];
+    $dataFive = $_POST['gender'];
+    $dataSix = $_POST['message'];
+
+    $fp = fopen('contact.txt', 'a');
+    fwrite($fp, $dataOne);
+    fwrite($fp, $dataTwo);
+    fwrite($fp, $dataThree);
+    fwrite($fp, $dataFour);
+    fwrite($fp, $dataFive);
+    fwrite($fp, $dataSix);
+
+    fclose($fp);
+}
 
 ?>
